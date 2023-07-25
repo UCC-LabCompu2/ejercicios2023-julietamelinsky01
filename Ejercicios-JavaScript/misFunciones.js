fc2d1cdd019cbdd64b6d332120b9562c06dc2488
julietamelinsky01 <2227956@ucc.edu.ar>
2023-07-25 00:13:36 -0300

    Cambio numero 1

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -49,3 +49,9 @@ function mostrar_ocultar(valorMO) {
 
     }
 }
+function calcular() {
+  var num1,num2;
+  num1=document.getElementsByName("sum_num1")[0].value;
+    num1=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("num_total")[0].value=num1+num2;
+}
